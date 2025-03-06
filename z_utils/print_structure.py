8be34ab46dfa4f07c312102ced998b65a37a411b
Liudaxia <aceliuchanghong@gmail.com>
2025-03-06 17:43:58 +0800

    feat: 模板添加

---
+++ z_utils/print_structure.py
@@ -0,0 +1,19 @@
+from tree_utils.struct_tree_out import print_tree
+
+path = "../my-python-templete"
+exclude_dirs_set = {
+    "z_utils",
+    ".env",
+    "LICENSE",
+    ".gitignore",
+    ".python-version",
+    "pyproject.toml",
+    "README.md",
+    "uv.lock",
+    "prompt.md",
+    "package-lock.json",
+    "node_modules",
+    "z_using_files",
+}
+print_tree(directory=path, exclude_dirs=exclude_dirs_set)
+# uv run z_utils/print_structure.py
